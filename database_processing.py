61e1f0f05f95e45b88ba5c202daae67b600ab398
Евгений Волчёк <zehrbit@gmail.com>
2024-06-07 14:51:48 +0300

    Исправил KeyError

--- database_processing.py
+++ database_processing.py
@@ -6,8 +6,9 @@ def update_db(data_from_form=None):
     car_list_from_MoySklad = []
     cars = utils.request_to_MoySklad_stores()
     for car in cars['rows']:
-        car_list_from_MoySklad.append(f"car_{car['name']}")
-        car_list_from_MoySklad.sort()  # [01, 02, 03...]
+        if car['name'].isdigit():
+            car_list_from_MoySklad.append(f"car_{car['name']}")
+            car_list_from_MoySklad.sort()  # [01, 02, 03...]
     conn = sqlite3.connect('car_status.db')
     cursor = conn.cursor()
     cursor.execute('CREATE TABLE IF NOT EXISTS car_status'
@@ -20,9 +21,9 @@ def update_db(data_from_form=None):
     for car_num in cars_from_DB:
         car_list_from_DB.append(car_num[0])
     for car_num in car_list_from_MoySklad:
-        try:
+        if data_from_form is not None:
             status = dict(data_from_form)[car_num]
-        except:
+        else:
             status = 1
         if car_num in car_list_from_DB:
             cursor.execute('UPDATE car_status SET status = ? WHERE car_num = ?', (status, car_num))
